076471eb13339bf6991cf634c4cf4c5e8d196b97
logeshkarthik <s.logeshkarthik@gmail.com>
2020-12-10 00:12:21 +0530

    php file added

---
+++ index.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- themify -->
+    <link href="asserts/icons/themify-icons/themify-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
+    <title>Logesh Karthik</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="">
+    <div style="background:black;" class="justify-content-center row
+            border-top">
+        <div class="pt-3 justify-content-center">
+            <ul class="row text-light">
+                <a href="#about" style="text-decoration: none;" class="text-white">
+                    <li class="col-3"><span class="ti-user"></span><br> About </li>
+                </a>
+                <a href="#skills" style="text-decoration: none;" class="text-white">
+                    <li class="col-3"> <span class="ti-stats-up"></span><br>Skills
+                    </li>
+                </a>
+                <a href="#works" style="text-decoration: none;" class="text-white">
+                    <li class="col-3"> <span class="ti-desktop"></span><br>Works</li>
+                </a>
+                <a href="#contact" style="text-decoration: none;" class="text-white">
+                    <li class="col-3"> <span class="ti-headphone"></span><br>Contact</li>
+                </a>
+
+
+            </ul>
+        </div>
+    </div>
+
+    <div style="background:black;opacity:0.5;" class="header">
+        <div class="p-2 bg-dark text-black container">
+            <h1>Hello !</h1>
+            <p>Iam Logesh Karthik</p>
+            <h6> 9551783347</h6>
+            <h6> s.logeshkarthik@gmail.com</h6>
+        </div>
+
+
+    </div>
+    <div class="bg-transparent container w-100 h-100 pt-5 pb-3
+            text-center h-20 border border-bottom border-right-0 border-top-0
+            border-left-0">
+        <h5 style="text-shadow: 2px 2px white;" class="text-black">Iam Living , Learning , Seeking oppurtunities etc..!</h5>
+    </div>
+
+    <div style="border-bottom:1px; background:grey;" class="row pb-3 pt-4
+            justify-content-center text-center">
+        <div class="col-4">
+            <a href="https://github.com/logeshkarthik">
+                    Check me
+                    <img src="asserts/images/github.svg" class="bg-white
+                        rounded-circle z-depth-0" alt="avatar image"
+                        height="35">
+                </a>
+
+        </div>
+        <div class="col-4">
+            <a href="https://www.linkedin.com/in/logesh-karthik-273862184/">
+                    Follow me
+                    <img src="asserts/images/linkedin.svg" class="bg-white
+                        z-depth-0" alt="avatar image" height="35">
+                </a>
+        </div>
+        <div class="col-4">
+            <a target="_blank" href="mailto:s.logeshkarthik@gmail.com">
+                    Reach me
+                    <img src="asserts/images/gmail.svg" class="z-depth-0"
+                        alt="avatar image" height="35">
+                </a>
+        </div>
+
+
+    </div>
+
+
+    <div id="about" style="background:black; opacity:0.5" class="content
+            justify-content-center text-center text-white container pt-3">
+
+        <p>I code, I create Web sites... explore Technologies... play outdoors! </p>
+        <p>Personally iam well- organised problem solver and a critical thinker.
+        </p>
+        <p>Interested in software industry and working on ambitious projects with positive people.</p>
+    </div>
+    <div id="skills" style="background:black; opacity:0.5" class="mt-1
+            content justify-content-center text-center text-white container
+            pt-3">
+        <h1> My Skils</h1>
+        <p>HTML, CSS,PHP, JS , building small and medium web apps, coding interactive layouts.</p>
+        <p>create designs.. Some content blablabla, some content blablabla.</p>
+        <p>Swipe right to view my skills!.</p>
+    </div>
+
+    <div class="scrolling-wrapper container">
+        <div class="card p-3 m-4">
+
+            <img src="asserts/images/html.png" alt="Girl in a jacket" width="50" height="50">
+        </div>
+        <div class="card p-3 m-4">
+            <img src="asserts/images/php.jpg" alt="Girl in a jacket" width="50" height="50">
+        </div>
+        <div class="card p-3 m-4">
+            <img src="asserts/images/My sql.png" alt="Girl in a jacket" width="50" height="50">
+        </div>
+        <div class="card p-3 m-4">
+            <img src="asserts/images/bootstrap.jpg" alt="Girl in a jacket" width="70" height="70">
+        </div>
+        <div class="card p-3 m-4">
+            <img src="asserts/images/js.jpg" alt="Girl in a jacket" width="50" height="50">
+        </div>
+        <div class="card p-3 m-4">
+            <img src="asserts/images/css.png" alt="Girl in a jacket" width="50" height="50">
+        </div>
+        <div class="card p-3 m-4">
+            <img src="asserts/images/excel.jpg" alt="Girl in a jacket" width="50" height="50">
+        </div>
+        <div class="card p-3 m-4">
+            <img src="asserts/images/figma.png" alt="Girl in a jacket" width="50" height="50">
+        </div>
+
+
+
+    </div>
+    <!-- works -->
+    <h1 id="works" class="text-center text-white">My Works</h1>
+    <div class="row p-3 justify-content-center text-center">
+
+        <!-- foox -->
+        <a href="https://foox.in/">
+            <div style="width:300px;height:150px;" class="card pt-3 row
+                    m-2">
+                <div class="row justify-content-center">
+                    <div class="col-4 text-center container"><img src="asserts/images/foox.png" alt="Girl in a
+                                jacket" width="80" height="80"></div>
+                    <div class="col justify-content-center text">A Web based Application for contactless dining by BeyondX Digital Technology. Technology explored: PHP, AJAX, MySQL.
+                    </div>
+                </div>
+
+            </div>
+        </a>
+        <!-- excel invoice -->
+        <a href="https://github.com/logeshkarthik/QuickPOS-using-excel">
+            <div style="width:300px;height:150px;" class="card pt-3 row
+                    m-2">
+                <div class="row justify-content-center">
+                    <div class="col-4 text-center"><img src="asserts/images/excel.jpg" alt="Girl in a
+                                jacket" width="80" height="80"></div>
+                    <div class="col justify-content-center text">Quick POS Application using Microsoft Excel - built for automating invoice calculation</div>
+
+                </div>
+
+            </div>
+        </a>
+
+        <!-- journal -->
+        <a href="http://www.ijitee.org/wp-content/uploads/papers/v9i3/B7820129219.pdf">
+            <div style="width:300px;height:150px;" class="card pt-3 row
+                    m-2">
+                <div class="row justify-content-center">
+                    <div class="col-4 text-center"><img src="asserts/images/journal.png" alt="Girl in a
+                                jacket" width="80" height="80"></div>
+                    <div class="col justify-content-center text">Journal paper - Intelligent system to identify unauthorized automobiles using internet of things</div>
+
+                </div>
+
+            </div>
+        </a>
+        <!-- fuel station  management excel -->
+        <a href="https://github.com/logeshkarthik/Fuel-station-management">
+            <div style="width:300px;height:150px;" class="card pt-3 row
+                    m-2">
+                <div class="row justify-content-center">
+                    <div class="col-4 text-center"><img src="asserts/images/excel.jpg" alt="Girl in a
+                                jacket" width="80" height="80"></div>
+                    <div class="col text justify-content-center">Fuel Station Management System using Microsoft Excel</div>
+                </div>
+
+            </div>
+        </a>
+
+        <!-- posenet -->
+        <a href="https://logeshkarthik.github.io/posenet-face-nose-identifier/">
+            <div style="width:300px;height:150px;" class="card pt-3 row
+                    m-2">
+                <div class="row justify-content-center">
+                    <div class="col-4 text-center"><img src="asserts/images/ml5.png" alt="Girl in a
+                                jacket" width="80" height="80"></div>
+                    <div class="col text justify-content-center">Face Nose Detection using Machine Learning - Web based application to spot nose position using ml5.js library.
+                    </div>
+
+                </div>
+
+            </div>
+        </a>
+
+    </div>
+
+    <!-- company iam experienced with -->
+    <h1 class="text-center text-white">Featured Organisations</h1>
+    <div class="row p-3 justify-content-center">
+        <a href="https://beyondx.in/">
+            <div class="card justify-content-center p-5 m-3">
+                <div class="row justify-content-center"> <img src="asserts/images/beyondx.jpeg" alt="Girl in a
+                            jacket" width="100" height="60"></div>
+                <div class="row mt-2 text-center">
+                    <h5>BeyondX Digital technologies pvt ltd</h5>
+                </div>
+
+            </div>
+        </a>
+        <a href="http://www.logicinstruments.in/">
+            <div class="card justify-content-center p-5 m-3">
+                <div class="row justify-content-center"> <img src="asserts/images/logic.jpg" alt="Girl in a
+                            jacket" width="200" height="60"></div>
+                <div class="row mt-2 text-center">
+                    <h5>Logic Instruments Chennai</h5>
+                </div>
+            </div>
+        </a>
+
+
+    </div>
+
+    <!-- contact -->
+    <h1 id="contact" class="text-center text-white">Contact</h1>
+    <div class="row justify-content-center text-center mr-0">
+        <form class="p-5">
+            <div class="row">
+                <div class="col-md-6 pb-2">
+                    <div class="">
+                        <input class="form-control" placeholder="enter your name" type="email">
+                    </div>
+
+                </div>
+                <div class="col-md-6 pb-2">
+                    <div class="">
+                        <input class="form-control" placeholder="enter your email" type="email">
+                    </div>
+
+                </div>
+
+
+            </div>
+            <div class="row pt-3 pb-4">
+                <div class="col">
+                    <div class="">
+                        <textarea class="form-control h-30 w-100" placeholder="enter your message" type="text"></textarea>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+            <div class="row text-center justify-content-center">
+                <button type="submit" class="btn-lg btn-dark">Send</button>
+            </div>
+
+
+
+        </form>
+
+    </div>
+
+    <!-- footer -->
+    <footer style="background:black" class="mainfooter
+            justify-content-center text-light">
+        <div class="row border-top">
+            <div class="pt-3 justify-content-center container">
+                <h5 class="text-center">Nothing is more expensive than a missed opportunity!</h5>
+            </div>
+        </div>
+        <div class="row">
+            <div class="pt-3 justify-content-center container">
+                <p class="text-center">-Logesh Karthik</p>
+            </div>
+        </div>
+
+    </footer>
+
+
+    <style>
+        body {
+            background-image: url('asserts/images/b1.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: 100% 100%;
+        }
+        
+        li {
+            display: inline;
+            text-align: center;
+        }
+        
+        .scrolling-wrapper {
+            overflow-x: scroll;
+            overflow-y: hidden;
+            white-space: nowrap;
+        }
+        
+        .text {
+            font-size: 14px;
+            text-align: left;
+        }
+        
+        .card {
+            padding: 5px;
+            display: inline-block;
+        }
+        
+        a {
+            text-decoration: none;
+            color: black;
+        }
+        /* Style the body */
+        
+        body {
+            font-family: Arial;
+            margin: 0;
+        }
+        /* Header/Logo Title */
+        
+        .header {
+            padding: 60px;
+            text-align: center;
+            background: #1abc9c;
+            color: white;
+            font-size: 30px;
+        }
+        /* Page Content */
+        
+        .content {
+            padding: 20px;
+        }
+    </style>
+</body>
+
+</html>
